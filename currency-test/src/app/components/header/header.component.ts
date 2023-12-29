223b5a33edeb27b6c8d8321d31f32f746d6b70dd
Hover19 <klaskintrade@gmail.com>
2023-12-29 18:00:51 +0200

    Create header and implement service with interval

--- currency-test/src/app/components/header/header.component.ts
+++ currency-test/src/app/components/header/header.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CurrencyApiService } from '../../core/services/currency-api.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  private currencyService = inject(CurrencyApiService);
 
+  public readonly monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  public readonly hryvna = 'Hryvna';
+
+  public currency$ = this.currencyService.currency$;
+
+  public currentDate = `${new Date().getDate()} ${
+    this.monthNames[new Date().getMonth()]
+  }, ${new Date().getFullYear()}`;
+
+  ngOnInit() {
+    this.currencyService.startPeriodicUpdates(600000).subscribe((data) => {
+      console.log('Updated data:', data);
+    });
+  }
 }
